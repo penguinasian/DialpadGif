f90973649255ee15e94ad6a3b8018a4af183d517
Daniel Zhang <zhangqiangtianchen@gmail.com>
2021-08-27 08:57:14 -0700

    Main.js

--- src/main.js
+++ src/main.js
@@ -1,4 +1,10 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
+const data = {
+    path: 'init',
+    client_uid: window.name,
+}
+window.sendMessage(data, 'https://dialpadbeta.com')
+
 createApp(App).mount('#app')
